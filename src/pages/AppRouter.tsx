235667ce8ce5062ef912490b3c8614ef10fe3a62
Eloy Cabañero <eloy_cs1992@hotmail.com>
2024-08-05 17:20:29 +0200

    feat: replace BrowserRouter to HashRouter

--- src/pages/AppRouter.tsx
+++ src/pages/AppRouter.tsx
@@ -2,16 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import CharactersPage from "./CharactersPage";
 import CharacterDetailPage from "./CharacterDetailPage";
 import NotFound from "./NotFound";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 const AppRouter = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<CharactersPage />} />
         <Route path="/character/:characterId" element={<CharacterDetailPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 export default AppRouter;
